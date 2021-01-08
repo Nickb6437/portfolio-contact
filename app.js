83ea4755a867fe0d1aace5aef3ce5c54e1ac255c
Nick Brown <nickb6437@outlook.com>
2021-01-08 17:19:54 +0000

    server update

--- app.js
+++ app.js
@@ -44,6 +44,11 @@ app.post("/email", (req,res) => {
 });
 
 
-app.listen(3000, function() {
-    console.log("Server running succesfully on port 3000!");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on succesfully");
 });
