102e0ec99276dc3e6b36f456b46033e61a48c1c7
Nick Brown <nickb6437@outlook.com>
2021-01-08 16:44:56 +0000

    inital commit

---
+++ app.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const express = require ("express");
+const bodyParser = require('body-parser');
+const cors = require ("cors");
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("API is running")
+});
+
+app.post("/email", (req,res) => {
+    const body = req.body.contact.message;
+    const name = req.body.contact.name;
+    const email = req.body.contact.email;
+
+    
+    
+    const msg = {
+    to: "nickb6437@outlook.com",
+    from: "nickb6437@outlook.com",
+    subject: name + " Portfolio Contact", 
+    text: "Email address " + email + " message = " + body
+    };
+
+    sgMail.send(msg, (err, res) => {
+        if(err) {
+            console.log(err)
+        }else {
+            console.log("Message Sent")
+        }
+    });
+
+    console.log(msg)
+
+});
+
+
+app.listen(3000, function() {
+    console.log("Server running succesfully on port 3000!");
+});
