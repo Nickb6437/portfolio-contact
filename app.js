a8e1c8373c016d8ed67e3763d2730b67b0adac3d
Nick Brown <nickb6437@outlook.com>
2021-01-15 16:39:30 +0000

    cors

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Origin", "https://nickb6437.github.io");
+    res.header("Access-Control-Allow-Headers", "content-type");
+    res.header("Access-Control-Allow-Credentials", true );
     next()
   });
 
